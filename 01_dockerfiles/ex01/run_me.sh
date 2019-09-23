16fc42543af801530902454905144ea7511a45ed
nkone <phtruong@protonmail.com>
2019-09-22 18:57:51 -0700

    fixed commands

--- 01_dockerfiles/ex01/run_me.sh
+++ 01_dockerfiles/ex01/run_me.sh
@@ -6,11 +6,11 @@
 #    By: phtruong <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/17 20:54:07 by phtruong          #+#    #+#              #
-#    Updated: 2019/09/18 15:28:28 by phtruong         ###   ########.fr        #
+#    Updated: 2019/09/22 18:55:28 by phtruong         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#!bin/sh
+#!/bin/sh
 docker build --no-cache -t teamspeak3 .
 docker run -itd --name teamspeak -p 9987:9987/udp -p 10011:10011 -p 30033:30033 --rm teamspeak3
 docker logs -f teamspeak
